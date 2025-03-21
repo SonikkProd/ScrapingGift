2be0549ff8be6db043913cd89ab4b754827df179
SonikProd <sonikprod.pro@gmail.com>
2025-03-21 13:36:28 +0100

    Remove Event

--- event.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-from PIL import Image
-from googletrans import Translator
-import io
-from tqdm import tqdm
-import colorama
-from colorama import Fore, Style
